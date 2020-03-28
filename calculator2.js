9ee365f8b4fbed38f98f8c478587c76a45c3fc19
bturtu405 <54838965+bturtu405@users.noreply.github.com>
2020-03-28 15:47:00 +0300

    update

--- calculator2.js
+++ calculator2.js
@@ -1,66 +1,67 @@
       const MAX_CHARACTERS_FOR_INT = 15;
       const MAX_CHARACTERS_INPUT = 20;
-      const MATH_ERROR = 'Math error';
+      const MATH_ERROR_MESSAGE = 'Math Error';
       const CALCULATOR = document.getElementById('calculator');
       const CALCULATOR_INPUT = document.getElementById('calculatorInput');
       const CALCULATOR_BUTTONS = {
-        'plus': '+',
-        'minus': '-',
-        'multiply': '*',
-        'divide': '/',
-        'equal': '=',
-        'clear': 'c',
-        'backspace': 'Backspace',
-        'zero': '0',
-        'one': '1',
-        'two': '2',
-        'three': '3',
-        'four': '4',
-        'five': '5',
-        'six': '6',
-        'seven': '7',
-        'eight': '8',
-        'nine': '9',
-        'dot': '.'
+        plus: '+',
+        minus: '-',
+        multiply: '*',
+        divide: '/',
+        equal: '=',
+        clear: 'c',
+        backspace: 'Backspace',
+        zero: '0',
+        one: '1',
+        two: '2',
+        three: '3',
+        four: '4',
+        five: '5',
+        six: '6',
+        seven: '7',
+        eight: '8',
+        nine: '9',
+        dot: '.'
       };
-      const CALCULATOR_OPERATOR_SIGNS = [CALCULATOR_BUTTONS['plus'], CALCULATOR_BUTTONS['minus'],
-        CALCULATOR_BUTTONS['divide'], CALCULATOR_BUTTONS['multiply']
+      const CALCULATOR_OPERATOR_SIGNS = [CALCULATOR_BUTTONS.plus, CALCULATOR_BUTTONS.minus,
+        CALCULATOR_BUTTONS.divide, CALCULATOR_BUTTONS.multiply
       ];
-      const CALCULATOR_FUNCTION_SIGN = [CALCULATOR_BUTTONS['equal'], CALCULATOR_BUTTONS['clear'],
-        CALCULATOR_BUTTONS['backspace'], 'Enter'
+      const CALCULATOR_ACTIONS_SIGNS = [CALCULATOR_BUTTONS.equal, CALCULATOR_BUTTONS.clear,
+        CALCULATOR_BUTTONS.backspace, 'Enter'
       ];
       let isNewEquation = true;
-      let operatorAppearances = [0];
+      let indexesOfOperators = [];
       CALCULATOR_INPUT.innerHTML = '';
-      let getClickedButtonId = (buttonId) => {
+
+      const clickedOnButton = (buttonId) => {
+        let button = document.getElementById(buttonId);
+        button.setAttribute('disabled', true);
+        button.removeAttribute('disabled');
         keySorter(CALCULATOR_BUTTONS[buttonId]);
       }
 
-      document.addEventListener('keydown', ({
-        key
-      }) => {
-        keySorter(key);
-      });
+      document.addEventListener('keydown', ({ key }) => { keySorter(key); });
 
-      let keySorter = (key) => {
-        if (CALCULATOR_FUNCTION_SIGN.includes(key)) {
-          calcFunctions(key);
+      const keySorter = (key) => {
+        if (CALCULATOR_ACTIONS_SIGNS.includes(key)) {
+          calcActions(key);
         } else {
-          if (!isNewEquation) {
-            clear();
-          }
-          if (CALCULATOR_INPUT.innerHTML.length < MAX_CHARACTERS_INPUT) {
+
+          if (isNewEquation && CALCULATOR_INPUT.innerHTML.length < MAX_CHARACTERS_INPUT) {
             writeInTextbox(key);
+            isNewEquation = true;
           }
         }
       }
 
-      let writeInTextbox = (key) => {
-        if (key.match(/[0-9]/) !== null) {
+      const writeInTextbox = (key) => {
+        checkForMinusInTheFirstIndex();
+
+        if (key.match(/[0-9]/) !== null && key.length === 1) {
           writeNumbers(key);
         }
 
-        if (key === CALCULATOR_BUTTONS['dot']) {
+        if (key === CALCULATOR_BUTTONS.dot) {
           writeDot();
         }
 
@@ -69,220 +70,210 @@
         }
       }
 
-      let writeNumbers = (key) => {
+      const writeNumbers = (key) => {
         equationInput = CALCULATOR_INPUT.innerHTML;
-        setAllIndexesOfOperators(equationInput);
-        let lastIndexOfOperator = operatorAppearances[operatorAppearances.length - 1];
+        setIndexesOfOperators(equationInput);
+        let lastIndexOfOperator = indexesOfOperators[indexesOfOperators.length - 1];
         let firstDigitOfNumber = equationInput.charAt(lastIndexOfOperator + 1);
-
-        if (equationInput === CALCULATOR_BUTTONS['zero']) {
+        if (equationInput === CALCULATOR_BUTTONS.zero) {
           backspace();
-        } else if (firstDigitOfNumber === CALCULATOR_BUTTONS['zero'] && lastIndexOfOperator > 0 &&
-          lastIndexOfOperator === equationInput.length - 2) {
+        } else if (firstDigitOfNumber === CALCULATOR_BUTTONS.zero && lastIndexOfOperator > 0
+          && lastIndexOfOperator === equationInput.length - 2) {
           backspace();
         }
         CALCULATOR_INPUT.innerHTML += key;
       }
 
-      let writeDot = () => {
+      const writeDot = () => {
         let equationInput = CALCULATOR_INPUT.innerHTML;
-        setAllIndexesOfOperators(equationInput);
-        let lastIndexOfOperator = operatorAppearances[operatorAppearances.length - 1];
+        setIndexesOfOperators(equationInput);
+        let lastIndexOfOperator = indexesOfOperators[indexesOfOperators.length - 1];
         if (!equationInput) {
-          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS['zero'] + CALCULATOR_BUTTONS['dot'];
+          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS.zero + CALCULATOR_BUTTONS.dot;
         } else if (CALCULATOR_OPERATOR_SIGNS.includes(equationInput.charAt(equationInput.length - 1))) {
-          CALCULATOR_INPUT.innerHTML += CALCULATOR_BUTTONS['zero'] + CALCULATOR_BUTTONS['dot'];
+          CALCULATOR_INPUT.innerHTML += CALCULATOR_BUTTONS.zero + CALCULATOR_BUTTONS.dot;
         } else if (!equationInput.slice(lastIndexOfOperator,
-            equationInput.length).includes(CALCULATOR_BUTTONS['dot'])) {
-          CALCULATOR_INPUT.innerHTML += CALCULATOR_BUTTONS['dot'];
+            equationInput.length).includes(CALCULATOR_BUTTONS.dot)) {
+          CALCULATOR_INPUT.innerHTML += CALCULATOR_BUTTONS.dot;
         }
       }
 
-      let writeOperator = (key) => {
+      const writeOperator = (key) => {
         let equationInput = CALCULATOR_INPUT.innerHTML;
         let lastCharacter = equationInput.charAt(equationInput.length - 1);
         if (!equationInput) {
-          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS['zero'] + key;
+          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS.zero + key;
         } else {
-          if (lastCharacter === CALCULATOR_BUTTONS['dot'] ||
-            CALCULATOR_OPERATOR_SIGNS.includes(lastCharacter)) {
+          if (lastCharacter === CALCULATOR_BUTTONS.dot
+            || CALCULATOR_OPERATOR_SIGNS.includes(lastCharacter)) {
             backspace();
           }
           CALCULATOR_INPUT.innerHTML += key;
         }
       }
 
-      let calcFunctions = (key) => {
-        if (key === 'Enter') {
-          key = '=';
-        }
-        const CALCULATOR_FUNCTIONS = {
-          'Backspace': backspace,
-          'c': clear,
-          '=': flipCalculator
-        };
-        CALCULATOR_FUNCTIONS[key]();
+      const calcActions = (key) => {
+        CALCULATOR_ACTIONS[key]();
       }
 
-      let setAllIndexesOfOperators = (equationInput) => {
-        while (operatorAppearances.length !== 1) {
-          operatorAppearances.pop();
-        }
-        for (var i = 1; i < equationInput.length; i++) {
-          if (CALCULATOR_OPERATOR_SIGNS.includes(equationInput.charAt(i))) {
-            operatorAppearances.push(i);
+      const setIndexesOfOperators = (equationInput) => {
+        indexesOfOperators = [];
+
+        for (let characterIndex = 1; characterIndex < equationInput.length; characterIndex++) {
+          if (CALCULATOR_OPERATOR_SIGNS.includes(equationInput.charAt(characterIndex))) {
+            indexesOfOperators.push(characterIndex);
           }
         }
       };
 
-      let equal = () => {
-          let equationInput = CALCULATOR_INPUT.innerHTML;
-          let lastCharacter = equationInput.charAt(equationInput.length - 1);
-          if (!equationInput) {
-            CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS['zero'];
+      const equal = () => {
+        flipCalculator();
+        let equationInput = CALCULATOR_INPUT.innerHTML;
+        let lastCharacter = equationInput.charAt(equationInput.length - 1);
+        if (!equationInput) {
+          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS.zero;
+        } else {
+          lastCharacterValid(lastCharacter);
+          let result = Number(getResult());
+          let isValidResult = validResult(result);
+          if (!isValidResult) {
+            mathError();
           } else {
-            if (lastCharacter.match(/[0-9]/) === null) {
-              backspace();
-            }
-            let result = getResult();
-            if (result.includes(null) || result.includes('e')) {
-              error(MATH_ERROR);
+            if (result.toString().length < MAX_CHARACTERS_INPUT) {
+              CALCULATOR_INPUT.innerHTML = result;
             } else {
-              if (result.length < MAX_CHARACTERS_INPUT) {
-                CALCULATOR_INPUT.innerHTML = result;
-              } else {
-                error(MATH_ERROR);
-              }
-
+              mathError();
             }
-            isNewEquation = false;
           }
+        }
       }
 
-      let getResult = () => {
+      const getResult = () => {
         let equationInput = CALCULATOR_INPUT.innerHTML;
-        let operatorIndex;
-        let equation;
         let result;
         let hasMultiplyOrDivideSigns;
-        setAllIndexesOfOperators(equationInput);
-        for (let i = 1; i < operatorAppearances.length; i++) {
-          operatorIndex = operatorAppearances[i];
-          equation = getTheEquationBetweenOperators(equationInput, i);
-          hasMultiplyOrDivideSigns = (equationInput.includes(CALCULATOR_BUTTONS['multiply']) ||
-            (equationInput.includes(CALCULATOR_BUTTONS['divide'])));
-          if (equationInput[operatorIndex] === CALCULATOR_BUTTONS['divide'] ||
-            equationInput[operatorIndex] === CALCULATOR_BUTTONS['multiply']) {
-            result = calculateMultiplyAndDivide(equation, equationInput[operatorIndex]);
-            i = 0;
-            equationInput = equationInput.replace(equation, result);
-            setAllIndexesOfOperators(equationInput);
-          } else if (!hasMultiplyOrDivideSigns) {
-            result = calculate(equation, equationInput[operatorIndex]);
-            i = 0;
-            equationInput = equationInput.replace(equation, result);
-            setAllIndexesOfOperators(equationInput);
+        setIndexesOfOperators(equationInput);
+        for (let operatorOrder = 0; operatorOrder < indexesOfOperators.length; operatorOrder++) {
+          hasMultiplyOrDivideSigns = (equationInput.includes(CALCULATOR_BUTTONS.multiply)
+          || (equationInput.includes(CALCULATOR_BUTTONS.divide)));
+          if (!hasMultiplyOrDivideSigns) {
+            operatorOrder = 0;
           }
-
+          operatorIndex = indexesOfOperators[operatorOrder];
+          if (operatorIndex > 0) {
+            equation = getTheEquationBetweenOperators(equationInput, operatorOrder);
+            if (equationInput[operatorIndex] === CALCULATOR_BUTTONS.divide
+              || equationInput[operatorIndex] === CALCULATOR_BUTTONS.multiply
+              || !hasMultiplyOrDivideSigns) {
+              result = calculate(equation, equationInput[operatorIndex]);
+              operatorOrder -= 2;
+              equationInput = equationInput.replace(equation, result);
+            }
+            setIndexesOfOperators(equationInput);
+          }
         }
         return equationInput;
       }
 
-      let calculateMultiplyAndDivide = (equation, operator) => {
-        let result;
-        if (equation.charAt(equation.length - 2) + equation.charAt(equation.length - 1) === '/0') {
-          return null;
+      const getTheEquationBetweenOperators = (equationInput, index) => {
+        if (index === 0) {
+          return (index < indexesOfOperators.length - 1) ? equationInput.slice(0, indexesOfOperators[1]) :
+            equationInput;
         } else {
-          return calculate(equation, operator);
+          return (index < indexesOfOperators.length - 1) ? equationInput.slice(indexesOfOperators[index - 1] + 1, indexesOfOperators[index + 1]) :
+            equationInput.slice(indexesOfOperators[index - 1] + 1, equationInput.length);
         }
       }
 
-      let getTheEquationBetweenOperators = (equationInput, index) => {
-        if (index === 1) {
-          return (index < operatorAppearances.length - 1) ? equationInput.slice(0, operatorAppearances[2])
-           : equationInput.slice(0, equationInput.length);
-        } else {
-          return (index < operatorAppearances.length - 1) ? equationInput.slice(operatorAppearances[index - 1] + 1, operatorAppearances[index + 1])
-          : equationInput.slice(operatorAppearances[index - 1] + 1, equationInput.length);
-        }
+      const backspace = () => {
+        if(isNewEquation){
+         CALCULATOR_INPUT.innerHTML = CALCULATOR_INPUT.innerHTML.slice(0, -1);
+       }
       }
 
-      let backspace = () => {
-        if (isNewEquation) {
-          CALCULATOR_INPUT.innerHTML =
-            CALCULATOR_INPUT.innerHTML.slice(0, CALCULATOR_INPUT.innerHTML.length - 1);
-        }
-      }
-
-      let clear = () => {
+      const clear = () => {
         CALCULATOR_INPUT.innerHTML = '';
         isNewEquation = true;
       }
 
-      let flipCalculator = () => {
+      const flipCalculator = () => {
         CALCULATOR.classList.add('rotated');
         CALCULATOR_INPUT.classList.add('hidden');
         setTimeout(() => {
           CALCULATOR_INPUT.classList.remove('hidden');
           CALCULATOR.classList.remove('rotated');
         }, 2000);
-        equal();
       }
 
-      let error = (error) => {
-        CALCULATOR_INPUT.innerHTML = error;
+      const mathError = () => {
+        CALCULATOR_INPUT.innerHTML = MATH_ERROR_MESSAGE;
+        isNewEquation = false;
         setTimeout(function() {
           clear();
         }, 3000);
       }
 
-      let calculate = (equation, operator) => {
-        let number1 = Number(equation.split(operator)[0]);
-        let number2 = Number(equation.split(operator)[1]);
-        let number1X10 = 10 * number1;
-        let number2X10 = 10 * number2;
-        let result;
-        let resultX10;
-        let timesMultipliedBy10;
+      const calculate = (equation, operator) => {
+        let firstNumber = Number(equation.split(operator)[0]);
+        let secondNumber = Number(equation.split(operator)[1]);
+        let timesMultipliedBy10 = multiplyTimes10ByNumberAfterDot(firstNumber, secondNumber);
+        firstNumber *= Math.pow(10, timesMultipliedBy10);
+        secondNumber *= Math.pow(10, timesMultipliedBy10);
         const MATH_OPERATOR = {
-          '+': () => {
-            result = number1 + number2;
-            resultX10 = number1X10 + number2X10;
-          },
-          '-': () => {
-            result = number1 - number2;
-            resultX10 = number1X10 - number2X10;
-          },
-          '*': () => {
-            result = number1 * number2;
-            resultX10 = number1X10 * number2X10;
-          },
-          '/': () => {
-            result = number1 / number2;
-            resultX10 = number1X10 / number2X10;
-          }
+          '+': () => {return firstNumber + secondNumber;},
+          '-': () => {return firstNumber - secondNumber;},
+          '*': () => {return firstNumber * secondNumber;},
+          '/': () => {return firstNumber / secondNumber;}
         };
-
-        for (timesMultipliedBy10 = 0; timesMultipliedBy10 < MAX_CHARACTERS_FOR_INT; timesMultipliedBy10++) {
-          MATH_OPERATOR[operator]();
-          if (result.toString().length <= MAX_CHARACTERS_FOR_INT ||
-            result.toString().includes(CALCULATOR_BUTTONS['dot'])) {
-            if (resultX10 / 100 === result) {
-              return divdeResult(result, timesMultipliedBy10 * 2);
-            } else if (resultX10 / 10 === result) {
-              return divdeResult(result, timesMultipliedBy10);
-            } else if (resultX10 === result) {
-              return result;
-            }
-            number1 = number1 * 10;
-            number2 = number2 * 10;
-            number1X10 = number1X10 * 10;
-            number2X10 = number2X10 * 10;
+        let result = MATH_OPERATOR[operator]();
+        if (operator === CALCULATOR_BUTTONS.multiply) {
+          return divdeResult(result, timesMultipliedBy10 * 2);
+        } else if (operator === CALCULATOR_BUTTONS.divide) {
+          if (secondNumber === 0) {
+            return false;
           }
+          return result;
+        } else {
+          return divdeResult(result, timesMultipliedBy10);
         }
-        return null;
       }
 
-      let divdeResult = (result, timesMultipliedBy10) => {
+      const divdeResult = (result, timesMultipliedBy10) => {
         return result / Math.pow(10, timesMultipliedBy10);
+      }
+
+      const multiplyTimes10ByNumberAfterDot = (firstNumber, secondNumber) => {
+        let firstNumberNumbersAfterdot = firstNumber.toString().split(CALCULATOR_BUTTONS.dot)[1];
+        let secondNumberNumbersAfterdot = secondNumber.toString().split(CALCULATOR_BUTTONS.dot)[1];
+        return firstNumberNumbersAfterdot !== undefined && secondNumberNumbersAfterdot !== undefined ? Math.max(firstNumberNumbersAfterdot.length, secondNumberNumbersAfterdot.length) :
+          firstNumberNumbersAfterdot !== undefined ? firstNumberNumbersAfterdot.length :
+          secondNumberNumbersAfterdot !== undefined ? secondNumberNumbersAfterdot.length :
+          0;
+      }
+
+      const CALCULATOR_ACTIONS = {
+        'Backspace': backspace,
+        'c': clear,
+        '=': equal,
+        'Enter': equal
+      };
+
+
+      const checkForMinusInTheFirstIndex = () => {
+        if (CALCULATOR_INPUT.innerHTML[0] === CALCULATOR_BUTTONS.minus) {
+          CALCULATOR_INPUT.innerHTML = CALCULATOR_BUTTONS.zero + CALCULATOR_INPUT.innerHTML;
+        }
+      }
+
+      const validResult = (result) => {
+        return (result.toString().length < MAX_CHARACTERS_FOR_INT
+        || result.toString().includes(CALCULATOR_BUTTONS.dot))
+        && !result.toString().includes(NaN)
+        && !result.toString().includes('e');
+      }
+
+      const lastCharacterValid = (lastCharacter) => {
+        if (lastCharacter.match(/[0-9]/) === null) {
+          backspace();
+        }
       }
